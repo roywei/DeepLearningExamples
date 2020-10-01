c513c7dc7c9a239ef1e5014f2069a14b2bc9d007
Lai Wei <royweilai@gmail.com>
2020-10-01 03:29:34 -0700

    add compute

--- PyTorch/Segmentation/MaskRCNN/pytorch/maskrcnn_benchmark/layers/iou_loss.py
+++ PyTorch/Segmentation/MaskRCNN/pytorch/maskrcnn_benchmark/layers/iou_loss.py
@@ -74,6 +74,63 @@ def bounded_iou_loss(pred, target, beta=0.2, eps=1e-3):
     return loss
 
 
+def bbox_transform(deltas, weights):
+    wx, wy, ww, wh = weights
+    dx = deltas[:, 0::4] / wx
+    dy = deltas[:, 1::4] / wy
+    dw = deltas[:, 2::4] / ww
+    dh = deltas[:, 3::4] / wh
+
+    dw = torch.clamp(dw, max=np.log(1000. / 16.))
+    dh = torch.clamp(dh, max=np.log(1000. / 16.))
+
+    pred_ctr_x = dx
+    pred_ctr_y = dy
+    pred_w = torch.exp(dw)
+    pred_h = torch.exp(dh)
+
+    x1 = pred_ctr_x - 0.5 * pred_w
+    y1 = pred_ctr_y - 0.5 * pred_h
+    x2 = pred_ctr_x + 0.5 * pred_w
+    y2 = pred_ctr_y + 0.5 * pred_h
+
+    return x1.view(-1), y1.view(-1), x2.view(-1), y2.view(-1)
+
+@weighted_loss
+def compute_iou(output, target):
+    output = output.float()
+    target = target.float()
+    transform_weights = (10., 10., 5., 5.)
+
+    x1, y1, x2, y2 = bbox_transform(output, transform_weights)
+    x1g, y1g, x2g, y2g = bbox_transform(target, transform_weights)
+
+    x2 = torch.max(x1, x2)
+    y2 = torch.max(y1, y2)
+
+    xkis1 = torch.max(x1, x1g)
+    ykis1 = torch.max(y1, y1g)
+    xkis2 = torch.min(x2, x2g)
+    ykis2 = torch.min(y2, y2g)
+
+    xc1 = torch.min(x1, x1g)
+    yc1 = torch.min(y1, y1g)
+    xc2 = torch.max(x2, x2g)
+    yc2 = torch.max(y2, y2g)
+
+    intsctk = torch.zeros(x1.size()).to(output)
+    mask = (ykis2 > ykis1) * (xkis2 > xkis1)
+    intsctk[mask] = (xkis2[mask] - xkis1[mask]) * (ykis2[mask] - ykis1[mask])
+    unionk = (x2 - x1) * (y2 - y1) + (x2g - x1g) * (y2g - y1g) - intsctk + 1e-7
+    iouk = intsctk / unionk
+
+    area_c = (xc2 - xc1) * (yc2 - yc1) + 1e-7
+    miouk = iouk - ((area_c - unionk) / area_c)
+    miouk = 1 - miouk
+
+    return miouk.half()
+
+
 @weighted_loss
 def giou_loss(pred, target, eps=1e-7):
     r"""`Generalized Intersection over Union: A Metric and A Loss for Bounding
@@ -352,7 +409,7 @@ class GIoULoss(nn.Module):
             assert weight.shape == pred.shape
             weight = weight.mean(-1)
 
-        loss = self.loss_weight * giou_loss(
+        loss = self.loss_weight * compute_iou(
             pred,
             target,
             weight,
