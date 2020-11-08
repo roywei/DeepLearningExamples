5292b3f413c60a1646a702306a0b184e959e3081
Ubuntu <ubuntu@ip-172-31-23-21.us-west-2.compute.internal>
2020-11-08 00:28:27 +0000

    fix

--- PyTorch/Segmentation/MaskRCNN/pytorch/maskrcnn_benchmark/engine/trainer.py
+++ PyTorch/Segmentation/MaskRCNN/pytorch/maskrcnn_benchmark/engine/trainer.py
@@ -135,9 +135,11 @@ def do_train(
         if per_iter_end_callback_fn is not None:
             early_exit = per_iter_end_callback_fn(iteration=iteration)
 
-            #output evaluation loss:
-            if iteration > 0 and iteration % args.iters_per_epoch == 0:
-
+            #let's not do this, there is a bug "No ground-truth boxes available for one of the images "
+            if False:#iteration > 0 and iteration % args.iters_per_epoch == 0:
+                
+                if is_main_process():
+                    print('Calculating evaluation loss.')
                 data_loader_val = make_data_loader(cfg, is_train=False, is_distributed=args.distributed)[0]
 
                 model.train()
@@ -151,8 +153,6 @@ def do_train(
                         images_val = images_val.to(cfg.MODEL.DEVICE)
                         targets_val = [target.to(cfg.MODEL.DEVICE) for target in targets_val]
                         loss_dict = model(images_val, targets_val)
-
-                        
                         losses = sum(loss for loss in loss_dict.values())
                         loss_dict_reduced = reduce_loss_dict(loss_dict)
                         losses_reduced = sum(loss for loss in loss_dict_reduced.values())
@@ -160,31 +160,13 @@ def do_train(
                 
                 synchronize()
 
-                logger = logging.getLogger("maskrcnn_benchmark.trainer")
-
-                logger.info(
-                    meters_val.delimiter.join(
-                        [
-                            "[Validation]: ",
-                            "eta: {eta}",
-                            "iter: {iter}",
-                            "{meters}",
-                            "lr: {lr:.6f}",
-                            "max mem: {memory:.0f}",
-                        ]
-                    ).format(
-                        eta=eta_string,
-                        iter=iteration,
-                        meters=str(meters_val),
-                        lr=optimizer.param_groups[0]["lr"],
-                        memory=torch.cuda.max_memory_allocated() / 1024.0 / 1024.0,
-                    )
-                )
-
+                if is_main_process():
+                    print('Save evaluation loss to tensorboard.')
+                
                 for name, meter in meters_val.meters.items():
                     args.writer.add_scalar('EvalMetrics/'+name, meter.global_avg, iteration / args.iters_per_epoch)
                 
-
+            
             if early_exit:
                 break
 
